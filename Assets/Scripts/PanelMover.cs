91189bb2be00d4227fc375a4a13f00e77e6e329f
Addison Yarborough <Addison.yarborough@youvisit.com>
2018-01-01 18:53:31 -0500

    Initial project commit

---
+++ Assets/Scripts/PanelMover.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PanelMover : UIManager
+{
+    public Canvas canvas;
+    public RectTransform[] panels;
+    public  bool canNavigate = true;
+    public int index;
+    float panelWidth;
+
+    void Start()
+    {
+        Init(canvas.GetComponent<RectTransform>().sizeDelta);
+    }
+
+    public void Init(Vector2 canvasSize)
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].anchorMin = panels[i].anchorMax = panels[i].pivot = Vector2.one * 0.5f;
+            panels[i].sizeDelta = new Vector2(panels[i].sizeDelta.x, canvasSize.y);
+            panels[i].gameObject.SetActive(i == index);
+        }
+
+        panelWidth = panels[0].sizeDelta.x;
+        panels[2].anchoredPosition = Vector2.zero;
+    }
+
+    public void NavigateToPanel(int fromPanel, int toPanel)
+    {
+        var isMovingLeft = true;
+
+        for (int i = 0; i < panels.Length; i++)
+            panels[i].gameObject.SetActive(i == fromPanel || i == toPanel);
+
+        if (fromPanel < toPanel || fromPanel == 4 && toPanel == 0)
+            isMovingLeft = false;
+
+        if (fromPanel == 0 && toPanel == 4)
+            isMovingLeft = true;
+
+        var toPanelStartPos = (isMovingLeft) ? panelWidth : -panelWidth;
+
+        StartCoroutine(MoveAnchorPositionX(panels[toPanel], toPanelStartPos, 0));
+        StartCoroutine(MoveAnchorPositionX(panels[toPanel], 0, 0.35f));
+        StartCoroutine(MoveAnchorPositionX(panels[fromPanel], toPanelStartPos, 0.35f, () =>
+                {
+                    panels[fromPanel].gameObject.SetActive(false);
+                    canNavigate = true;
+                }));
+
+
+        // panels[toPanel].DOAnchorPosX(toPanelStartPos, 0);
+        // panels[fromPanel].DOAnchorPosX(isMovingLeft ? -panelWidth : panelWidth, AnimationSpeeds.fast).OnComplete(() =>
+        //   {
+        //      panels[fromPanel].gameObject.SetActive(false);
+        //      canNavigate = true;
+        //  });
+        //  panels[toPanel].DOAnchorPosX(0, AnimationSpeeds.fast);
+    }
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    /// <summary>
+    /// Fade the specified image's alpha to the targetAlpha over duration,
+    /// and optionally call back via OnComplete.
+    /// </summary>
+    /// <param name="img">Image.</param>
+    /// <param name="targetAlpha">Target alpha.</param>
+    /// <param name="duration">Duration.</param>
+    /// <param name="OnComplete (optional)">On complete.</param>
+    public virtual   IEnumerator Fade(Image img, float targetAlpha, float duration, Action OnComplete = null)
+    {
+        var elapsed = 0f;
+        var startColor = img.color;
+        var targetColor = new Color(startColor.r, startColor.g, startColor.b, targetAlpha);
+
+        while (elapsed < duration)
+        {
+            img.color = Color.Lerp(startColor, targetColor, (elapsed / duration));
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        if (OnComplete != null)
+            OnComplete();
+    }
+
+    /// <summary>
+    /// Fade the specified canvasGroup's alpha to the targetAlpha over duration,
+    /// and optionally call back via OnComplete.
+    /// </summary>
+    /// <param name="canvasGroup">Canvas group.</param>
+    /// <param name="targetAlpha">Target alpha.</param>
+    /// <param name="duration">Duration.</param>
+    /// <param name="OnComplete (optional)">On complete.</param>
+    public virtual   IEnumerator Fade(CanvasGroup canvasGroup, float targetAlpha, float duration, Action OnComplete = null)
+    {
+        var elapsed = 0f;
+        var startAlpha = canvasGroup.alpha;
+
+        while (elapsed < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, (elapsed / duration));
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        if (OnComplete != null)
+            OnComplete();
+    }
+
+    /// <summary>
+    /// Scale the specified transform to targetScale over duration,
+    /// and optionally call back via OnComplete.
+    /// </summary>
+    /// <param name="t">T.</param>
+    /// <param name="targetScale">Target scale.</param>
+    /// <param name="duration">Duration.</param>
+    /// <param name="OnComplete (optional)">On complete.</param>
+    public virtual  IEnumerator Scale(Transform t, Vector3 targetScale, float duration, Action OnComplete = null)
+    {
+        var elapsed = 0f;
+        var startScale = t.localScale;
+
+        while (elapsed < duration)
+        {
+            t.localScale = Vector3.Lerp(startScale, targetScale, (elapsed / duration));
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        if (OnComplete != null)
+            OnComplete();
+    }
+
+    /// <summary>
+    /// Moves the anchor position of the specified rectTransform's X value to targetPosX over duration,
+    /// and optionally calls back via OnComplete.
+    /// </summary>
+    /// <returns>The anchor position.</returns>
+    /// <param name="t">T.</param>
+    /// <param name="targetPos">Target position.</param>
+    /// <param name="duration">Duration.</param>
+    /// <param name="OnComplete (optional)">On complete.</param>
+    public virtual IEnumerator MoveAnchorPositionX(RectTransform t, float targetPosX, float duration, Action OnComplete = null)
+    {
+        var elapsed = 0f;
+        var startPos = t.anchoredPosition;
+        var targetPos = new Vector2(targetPosX, t.anchoredPosition.y);
+
+        while (elapsed < duration)
+        {
+            t.anchoredPosition = Vector2.Lerp(startPos, targetPos, (elapsed / duration));
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        if (OnComplete != null)
+            OnComplete();
+    }
+}
